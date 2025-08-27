4741290cee21fed69f4a41f6fac158ebcf4c6f37
Florian Lardeux <magnaflo35@gmail.com>
2025-08-27 18:12:51 +0200

    First draft

--- Pulse/ContentView.swift
+++ Pulse/ContentView.swift
@@ -1,21 +1,38 @@
-//
-//  ContentView.swift
-//  Pulse
-//
-//  Created by Florian Lardeux on 20/08/2025.
-//
 
 import SwiftUI
 
+func test(data: PulseData) -> String {
+    return String(data.t0)
+}
+
 struct ContentView: View {
+    
+    var data = PulseData()
+    @State var text = "0"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                let t = Float(DispatchTime.now().uptimeNanoseconds)
+                data.append(t: t)
+                if data.time.count > 0 {
+                    text = String(data.time[data.time.count-1])
+                }
+                print(data.time)
+                
+            } label: {
+                Text("Pulse")
+                    .frame(width: 200, height: 200)
+                    .font(.system(size: 40, weight: .semibold, design: .default))
+                    .foregroundColor(.white)
+                    .background(.red)
+                    .cornerRadius(100)
+            }
+            
+            Text(text)
+                .font(.system(size: 50))
+                .foregroundColor(Color(.label))
         }
-        .padding()
     }
 }
 
